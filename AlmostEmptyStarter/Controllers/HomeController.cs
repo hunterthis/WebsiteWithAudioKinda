1965d8ca6e564bf65924a327a9bafda7786a1acb
Hunter <46310167+hunterthis@users.noreply.github.com>
2022-07-12 19:14:04 -0700

    Add project files.

---
+++ AlmostEmptyStarter/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlmostEmptyStarter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // sets title 
+            ViewData["Title"] = "Home Page";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["About"] = "About";
+            return View();
+        }
+        public IActionResult Audio()
+        {
+            ViewData["Audio"] = "Audio";
+            return View();
+        }
+
+
+    }
+}
